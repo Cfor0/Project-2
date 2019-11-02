d846b35b7f5e95d8881c7fdd44c875f99f4ca895
ShawnG <shawngway@gmail.com>
2019-11-02 13:16:02 -0400

    admin js

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,97 +1,37 @@
-let $placeNameInput = $("#placeNameInput");
-let $addressInput = $("#addressInput");
-let $deliveryTime = $("#deliveryTime");
-let $deliveryDay = $("#deliverDay");
-let $customerButton = $("#customerButton");
 
-const API = {
-  saveCustomer: function(customer) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/jon"
-      },
-      type: "POST",
-      url: "api/customers",
-      data: JSON.stringify(customer)
-    });
-  },
-  getCustomer: function() {
-    return $.ajax({
-      url: "api/customers",
-      type: "GET"
-    });
-  },
-  deleteCustomer: function(id) {
-    return $.ajax({
-      url: "api/customer/" + id,
-      type: "DELETE"
-    });
-  }
-};
+$(document).ready(function () {
 
-const refreshCustomers = function() {
-  API.getCustomers().then(function(data) {
-    const $customers = data.map(function(customer) {
-      const $a = $("<a>")
-        .text(customer.name)
-        .attr("href", "/customer/" + customer.id);
 
-      const $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": customer.id
-        })
-        .append($a);
+    let customerName = $("#placeNameInput");
+    let address = $("#addressInput");
+    let deliveryTime = $("#deliveryTime");
+    let deliveryDay = $("#deliveryDay");
+    let customerButton = $(".customerButton");
+    var customerList = $("#customer-list");
 
-      const $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text(" x ");
+    $(".createForm").on("submit", function(event){
+        event.preventDefault();
 
-      $li.append($button);
-
-      return $li;
-    });
-
-    $customerList.empty();
-    $customerList.append($customers);
-  });
-};
-
-const customerFormSubmit = function(event) {
-  event.preventDefault();
-
-  let customer = {
-    name: $placeNameInput.val().trim(),
-    address: $addressInput.val().trim(),
-    deliveryTime: $deliveryTime.val().trim(),
-    day: $deliveryDay.val().trim()
-  };
-  if (!(customer.name && customer.address && customer.deliveryTime && day)) {
-    alert(
-      "You must enter have a delivery name, address, delivery time, and a day"
-    );
-    return;
-  }
-
-  API.saveCustomer(customer).then(function() {
-    refreshCustomers();
-  });
-
-  $placeNameInput.val("");
-  $addressInput.val("");
-  $deliveryTime.val("");
-  $deliveryDay.val("");
-};
-
-const handleDeleteBtnClick = function() {
-  const idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  API.deleteExample(idToDelete).then(function() {
-    refreshCustomers();
-  });
-};
-
-$customerButton.on("click", customerFormSubmit);
-$customerList.ong("click", ".delete", handleDeleteBtnClick);
+        var newCustomer = {
+            name: customerName.val().trim(),
+            address: address.val().trim(),
+            deliveryTime: deliveryTime.val().trim(),
+            day: deliveryDay.val().trim()
+        }
+        // $.ajax("/api/customer", {
+        //     type: "POST",
+        //     data: newCustomer
+        // }).then(
+        //     function(){
+        //         console.log(data);
+        //     }
+        // )
+        function upsertCustomer (newCustomer) {
+            $.post("/api/customer", newCustomer)
+            .then(
+                console.log(newCustomer)
+            )
+        }
+        upsertCustomer();   
+    })
+});
